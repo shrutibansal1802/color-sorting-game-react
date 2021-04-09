9737223c8bb7faa978436472b0bc2929930203bb
Shruti Bansal <shrutibansal1802@gmail.com>
2021-04-09 20:23:35 +0530

    swap colors implemented

--- src/App.js
+++ src/App.js
@@ -4,45 +4,63 @@ import chroma, { hsl } from 'chroma-js'
 import randomColor from 'randomcolor'
 
 function App() {
-  const[color, setColor] = useState('')
-  const [error,setError] = useState(false);
-  const [list,setList] = useState(chroma.scale(['#080b5c','#2a4858']).colors(20))
+  const [first, setFirst] = useState();
+  const [second, setSecond] = useState()
+  const [oneselected, setOneselected] = useState(false);
+  const [twoselected, setTwoselected] = useState(false);
 
-  function generateRandomColor(luminous){
-    let newcolor = `${randomColor({luminosity:luminous})}`
+  const [list, setList] = useState(chroma.scale(['#080b5c', '#faa']).colors(20))
+
+  function generateRandomColor(luminous) {
+    let newcolor = `${randomColor({ luminosity: luminous })}`
     console.log(newcolor)
     return newcolor;
   }
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    let color1= generateRandomColor('light');
+    let color1 = generateRandomColor('light');
     let color2 = generateRandomColor('dark');
 
-    let colors = chroma.scale([color1,color2]).colors(25)
+    let colors = chroma.scale([color1, color2]).colors(25)
+    const correctList = colors;
     setList(colors)
   }
+  function swap() {
+    let newList = list
+    if (oneselected && twoselected) {
+      [newList[second] ,newList[first]]=[newList[first] , newList[second]]
+    }
+    setList(newList)
+    console.log(list)
+    setOneselected(false)
+    setTwoselected(false)
+  }
 
+  useEffect(() => { swap() }, [twoselected])
+
+ 
   return (
     <>
-   
-    <section className='container'>
-      <h3>color generator</h3>
-      <form onSubmit={handleSubmit}>
-        {/* <input type='text' value={color} 
-        onChange={(e)=>{setColor(e.target.value)}} 
-        placeholder='#f15025' 
-        className={`${error? 'error': null}`}>
-        </input> */}
-        <button className='btn' type='submit'>Generate</button>
-      </form>
-    </section>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <button className='btn' type='submit'>Generate</button>
+        </form>
+      </section>
 
-    <section className='colors'>
-      {list.map((color, index) =>{
-        return <SingleColor key={index}gethex={color} index={index}></SingleColor>
-      })}
-    </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return <SingleColor key={index} gethex={color}
+            index={index}
+            setFirst={setFirst}
+            setSecond={setSecond}
+            oneselected={oneselected}
+            twoselected={twoselected}
+            setOneselected={setOneselected}
+            setTwoselected={setTwoselected}></SingleColor>
+        })}
+      </section>
     </>
   )
 }
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,15 +2,34 @@
 import React, { useState, useEffect } from 'react'
 
 
-const SingleColor = ({ gethex, index }) => {
+const SingleColor = ({ gethex, index ,setFirst, setSecond, oneselected,twoselected,setOneselected ,setTwoselected } ) => {
+  const [alert, setAlert] = useState(false)
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [alert])
 
   return (
-    <article className={`color`} style={
-      { backgroundColor: `${gethex}` }}>
+    <article key={index} 
+    className ={alert?'color-active': 'color'}
+    style={
+      { backgroundColor: `${gethex}` }}
+      onClick ={()=>{navigator.clipboard.writeText(gethex)
+        setAlert(true)
+       if(oneselected)
+       {setSecond(index)
+      setTwoselected(true)}
+      else{
+        setFirst(index)
+        setOneselected(true)
+      }
+      console.log(twoselected,oneselected)
+      }
+       }>
 
-      {/* <p>hello</p> */}
-     {/* {console.log(gethex)} */}
     </article>
   )
 }
